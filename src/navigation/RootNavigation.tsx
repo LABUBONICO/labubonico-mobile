1ee28ba158e59d0b9a269e179c89486229a2fb42
Raul <rclauson141@gmail.com>
2025-10-27 10:02:03 -0300

    feat: update authentication methods and add Google Sign-In support
    - Changed Expo start commands for Android and iOS to use `expo run`.
    - Added dependencies for AsyncStorage and Google Sign-In.
    - Updated Firebase configuration to use AsyncStorage for authentication persistence.
    - Enhanced AuthContext to include Google Sign-In functionality and improved error handling.
    - Modified RootNavigation to show a loading indicator while checking user authentication state.
    - Updated Login screen to include loading state and error messages, and added Google Sign-In button.
    - Adjusted TypeScript configuration to include paths for Firebase authentication types.

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -2,9 +2,17 @@ import { useContext, useState } from "react";
 import MainStack from "./MainStack";
 import { AuthContext } from "../contexts/AuthContext";
 import Login from "../screens/Login";
+import { ActivityIndicator, View } from "react-native";
 
 const RootNavigation = () => {
-  const { user } = useContext(AuthContext);
+  const { user, loadingUser } = useContext(AuthContext);
+  if (loadingUser) {
+    return (
+      <View>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+  }
   return user ? <MainStack /> : <Login />;
 };
 
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,18 @@
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import styles from "../styles";
 import { useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 const Login = () => {
-  const { login, register } = useContext(AuthContext);
+  const { login, register, loginWithGoogle, errorMSG, setErrorMSG, loading } =
+    useContext(AuthContext);
   const [signup, setSignup] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -24,17 +32,21 @@ const Login = () => {
               style={styles.input}
               placeholder="Nome"
               onChangeText={setName}
+              onFocus={() => setErrorMSG("")}
             />
           )}
           <TextInput
             style={styles.input}
             placeholder="Email"
             onChangeText={setEmail}
+            onFocus={() => setErrorMSG("")}
           />
           <TextInput
             style={styles.input}
             placeholder="Senha"
             onChangeText={setPassword}
+            onFocus={() => setErrorMSG("")}
+            secureTextEntry
           />
         </View>
         <TouchableOpacity
@@ -42,10 +54,15 @@ const Login = () => {
             signup ? register(name, email, password) : login(email, password);
           }}
         >
-          <Text>{signup ? "Cadastrar" : "Entrar"}</Text>
+          {loading ? (
+            <ActivityIndicator />
+          ) : (
+            <Text>{signup ? "Cadastrar" : "Entrar"}</Text>
+          )}
         </TouchableOpacity>
+        {errorMSG && <Text>{errorMSG}</Text>}
         <Text>OU</Text>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={loginWithGoogle}>
           <Text>Entrar com o Google</Text>
         </TouchableOpacity>
       </View>
